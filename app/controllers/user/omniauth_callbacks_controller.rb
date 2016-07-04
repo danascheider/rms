c0eca404a9bc5aa92631a83979e3d29a5def5c9c
Dana Scheider <dana.scheider@gmail.com>
2016-07-04 13:39:00 -0700

    Make this reeeaaalllll simple

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -2,15 +2,16 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # You should also create an action method in this controller like this:
   def google_oauth2
-    @user = ::User.from_omniauth(request.env["omniauth.auth"])
+    redirect_to root_path
+    # @user = ::User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to root_path
-    end
+    # if @user.persisted?
+    #   sign_in_and_redirect @user, :event => :authentication
+    #   set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+    # else
+    #   session["devise.facebook_data"] = request.env["omniauth.auth"]
+    #   redirect_to root_path
+    # end
   end
 
   # More info at:
