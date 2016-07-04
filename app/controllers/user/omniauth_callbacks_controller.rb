8b4c95d8bd0baeae7349fa79cdd69111c6a9675a
Dana Scheider <dana.scheider@gmail.com>
2016-07-04 13:22:53 -0700

    Reroute to root path on failure

--- app/controllers/user/omniauth_callbacks_controller.rb
+++ app/controllers/user/omniauth_callbacks_controller.rb
@@ -2,16 +2,15 @@ class User::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # You should also create an action method in this controller like this:
   def google_oauth2
-    byebug
-    # @user = ::User.from_omniauth(request.env["omniauth.auth"])
+    @user = ::User.from_omniauth(request.env["omniauth.auth"])
 
-    # if @user.persisted?
-    #   sign_in_and_redirect @user, :event => :authentication
-    #   set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
-    # else
-    #   session["devise.facebook_data"] = request.env["omniauth.auth"]
-    #   redirect_to new_user_url
-    # end
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to root_path
+    end
   end
 
   # More info at:
